8df1f999bac9e060141744b091419476e3444666
Khadija Atia <sanjidan1993@gmail.com>
2021-08-09 02:58:38 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rapoo';
+  title = 'rapoo - It solutions |  Corporate template';
+
+  
+
+  ngOnInit(){
+    
+  }
 }
