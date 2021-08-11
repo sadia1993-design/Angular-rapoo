b375bdbc9dcdac352c97cda02280ea0257fa90d4
Khadija Atia <sanjidan1993@gmail.com>
2021-08-12 04:38:45 +0600

    routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ import { ProjectsComponent } from './Component/projects/projects.component';
 import { PricingComponent } from './Component/pricing/pricing.component';
 import { SingleBlogComponent } from './Component/single-blog/single-blog.component';
 import { SingleProjectsComponent } from './Component/single-projects/single-projects.component';
+import { ErrorComponent } from './Component/error/error.component';
+import { Layout1Component } from './Component/Layout/layout1/layout1.component';
+import { Layout2Component } from './Component/Layout/layout2/layout2.component';
+import { ContactComponent } from './Component/contact/contact.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
@@ -23,3 +27,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [HomeComponent, AboutComponent,ServicesComponent,ProjectsComponent,PricingComponent,SingleBlogComponent,SingleProjectsComponent,ErrorComponent,Layout1Component,Layout2Component,ContactComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HomeComponent } from './Component/home/home.component';
-import { AboutComponent } from './Component/about/about.component';
-import { ServicesComponent } from './Component/services/services.component';
-import { PricingComponent } from './Component/pricing/pricing.component';
-import { ProjectsComponent } from './Component/projects/projects.component';
-import { ContactComponent } from './Component/contact/contact.component';
-import { ErrorComponent } from './Component/error/error.component';
-import { SingleBlogComponent } from './Component/single-blog/single-blog.component';
-import { SingleProjectsComponent } from './Component/single-projects/single-projects.component';
-import { Layout1Component } from './Component/Layout/layout1/layout1.component';
-import { Layout2Component } from './Component/Layout/layout2/layout2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ServicesComponent,
-    PricingComponent,
-    ProjectsComponent,
-    ContactComponent,
-    ErrorComponent,
-    SingleBlogComponent,
-    SingleProjectsComponent,
-    Layout1Component,
-    Layout2Component
+    routingComponents
   ],
   imports: [
     BrowserModule,
