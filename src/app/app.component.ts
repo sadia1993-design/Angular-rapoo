f82165a68110664da2a2ef22bee3310319c8b1e9
Khadija Atia <sanjidan1993@gmail.com>
2021-08-12 02:58:57 +0600

    jquery added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component, AfterViewInit  } from '@angular/core';
+import { Component, OnInit    } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rapoo - It solutions |  Corporate template';
 
   
@@ -17,7 +18,7 @@ export class AppComponent {
   ngAfterViewInit(){
     $(document).ready(function(){
       $("p").click(function(){
-      $(this).hide();
+      $("p").hide();
       });
     });
 }
